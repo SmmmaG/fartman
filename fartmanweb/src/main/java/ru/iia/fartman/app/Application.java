1e74336e50f544e48d716be42ac26aa26d9e7449
IAIsmakaev <IAIsmakaev@greenatom.ru>
2018-04-12 18:12:48 +0300

    add rest sample

---
+++ fartmanweb/src/main/java/ru/iia/fartman/app/Application.java
@@ -0,0 +1,24 @@
+package ru.iia.fartman.app;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import ru.iia.fartman.site.services.LigaConfiger;
+
+@SpringBootApplication
+@ComponentScan("ru.iia")
+public class Application {
+   private static final Logger logger = LogManager.getLogger(Application.class);
+
+    @Autowired
+    private ApplicationContext context;
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
